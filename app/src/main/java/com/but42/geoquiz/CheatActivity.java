654d7e960a2b505c2a8647db69d78e91a4055ee4
Ted <but42@bk.ru>
2016-10-15 18:35:04 +0300

    Add shown api level

--- app/src/main/java/com/but42/geoquiz/CheatActivity.java
+++ app/src/main/java/com/but42/geoquiz/CheatActivity.java
@@ -2,6 +2,7 @@ package com.but42.geoquiz;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -32,6 +33,9 @@ public class CheatActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+        TextView apiVersion = (TextView) findViewById(R.id.APIVersion);
+        apiVersion.append("API level " + Build.VERSION.SDK_INT);
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
